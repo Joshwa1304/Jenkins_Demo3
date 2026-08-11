072b8b884d901c652284b4507de72bc5bf1bd1a9
Joshwa <joshwa@vrnexgen1.com>
2026-08-11 16:39:58 +0530

    Import Scanner

--- Hello.java
+++ Hello.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, DevOps!");
 
-        int age = 10;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your age");
+        int age = sc.nextInt();
 
         if (age >= 5) {
             System.out.println("You are eligible");
