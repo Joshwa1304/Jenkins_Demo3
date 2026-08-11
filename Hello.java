89d40f1c674f7a6079699cf549bb3be767c9279c
Joshwa <joshwa@vrnexgen1.com>
2026-08-11 16:32:07 +0530

    Modified

--- Hello.java
+++ Hello.java
@@ -2,5 +2,15 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, DevOps!");
+
+        int age = 10;
+
+        if (age >= 5) {
+            System.out.println("You are eligible");
+        }
+
+        else {
+            System.out.println("You are not eligible");
+        }
     }
 }
