f400d51fa6b649768e7f3076fbc11f57ce58737e
Joshwa <joshwa@vrnexgen1.com>
2026-09-02 11:39:04 +0530

    Create new HCL component from Jenkins

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Hello, Jenkins!");
         System.out.println("Hello, DevOps!");
 
         int age = 10;
